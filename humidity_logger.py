6e945ba9206a6f1a8487867c8ff900c3c9acf27f
Anton Kühn <112161517+Ironflag420@users.noreply.github.com>
2022-10-19 12:09:21 +0200

    Update humidity_logger.py

--- humidity_logger.py
+++ humidity_logger.py
@@ -10,7 +10,7 @@ try:
 except:
  pass
 while True:
- humidity, temperature = Adafruit_DHT.red_retry(DHT_SENSOR, DHT_PIN)
+ humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 if humidity is not None and temperature is not None:
  f.write('{0},{1}, {2:0.1f}*C,{3:0.1f)%\r\n'.format(time.strftime('%m/%d/%y'), time.strftime('%H:%M), temperature, humidity))
 else:
